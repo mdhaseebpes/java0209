5f23b832301c5d7c3889882b5b27db2264c7ffae
Mohammed Haseeb <mohammed.ali@ymedialabs.com>
2021-12-19 22:47:07 +0530

    resolved conflict

--- src/expressionStatementMethods/StashMergeConflictGit.java
+++ src/expressionStatementMethods/StashMergeConflictGit.java
@@ -1,13 +1,16 @@
+package expressionStatementMethods;
+
 public class StashMergeConflictGit {
     public static void main(String[] args) {
         //   master d1 
+
         double d= 234.12413;
         String text = Double.toString(Math.abs(d));
         int integerPlaces = text.indexOf('.');
         int decimalPlaces = text.length() - integerPlaces - 1;
         System.out.println(decimalPlaces);
-        System.out.println("master d1 branch  changes");
-        System.out.println("master d1 branch  commit for cherry pick");
+        System.out.println("local d2 branch  changes");
+        System.out.println("local d2 branch  commit for cherry pick");
 
     }
 
